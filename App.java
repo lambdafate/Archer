765f3e25aaad0c5e7b56ffd62a20125fd4b58a88
lambdafate <lambdafate@163.com>
2020-05-27 23:23:32 +0800

    run this demo in Archer!

---
+++ App.java
@@ -0,0 +1,37 @@
+import Archer.*;
+
+/**
+ * App: for testing archer
+ */
+
+public class App {
+
+    @Archer.router(path="/login", method={"GET", "POST"})
+    public static Object login(){
+        if(islogin()){
+            Response.redirect_to("/");
+        }
+        if(Request.method().equals("GET")){
+            return "login.html";
+        }
+        Session.set("login", true);
+        return Response.redirect_to("/");
+    }
+
+    @Archer.router(path="/", method={"GET"})
+    public static Object index(){
+        if(!islogin()){
+            return Response.redirect_to("/login");
+        }
+        return "index.html";
+    }
+
+    public static boolean islogin(){
+        return Session.get("login") != null && (Boolean)Session.get("login");
+    }
+
+    public static void main(String[] args) {
+        var archer = new Archer(App.class);
+        archer.run(8888);
+    }
+}
